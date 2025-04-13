8a015dbd67283c61b89deaefc940493e8c00bcc1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-13 14:50:17 +0200

    Add documentation

--- BIBFRAME2MARC.pm
+++ BIBFRAME2MARC.pm
@@ -72,3 +72,143 @@ sub transform {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+NKC::Transform::BIBFRAME2MARC - bibframe2marc transformation class.
+
+=head1 SYNOPSIS
+
+ use NKC::Transform::BIBFRAME2MARC;
+
+ my $obj = NKC::Transform::BIBFRAME2MARC->new(%params);
+ my $version = $obj->version;
+ my $marc_xml = $obj->transform($bf_xml);
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = NKC::Transform::BIBFRAME2MARC->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<version>
+
+Transformation version.
+
+Default value is '2.6.0'.
+
+Default value is undef.
+
+=item * C<xslt_transformation_file>
+
+XSLT transformation file.
+
+Default value is XSLT transformation file for '2.6.0' version.
+
+=back
+
+Returns instance of object.
+
+=head2 C<version>
+
+ my $version = $obj->version;
+
+Get bibframe2marc transformation version which is set to object.
+
+Returns qr{\d\.\d\.\d} version string.
+
+=head2 C<transform>
+
+ my $marc_xml = $obj->transform($bf_xml);
+
+Transform BIBFRAME to MARC.
+
+Returns MARC XML string.
+
+=head1 ERRORS
+
+ new():
+         Cannot read XSLT file.
+                 XSLT file: %s
+         Parameter 'version' is undefined.
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+
+=head1 EXAMPLE
+
+=for comment filename=get_version.pl
+
+ use strict;
+ use warnings;
+
+ use NKC::Transform::BIBFRAME2MARC;
+
+ # Object.
+ my $obj = NKC::Transform::BIBFRAME2MARC->new;
+
+ # Get version.
+ my $version = $obj->version;
+
+ # Print out.
+ print $version."\n";
+
+ # Output:
+ # 2.6.0
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<File::Share>,
+L<XML::LibXML>,
+L<XML::LibXSLT>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<NKC::Transform::MARC2BIBFRAME>
+
+marc2bibframe transformation class.
+
+=item L<NKC::Transform::MARC2RDA>
+
+marc2rda transformation class.
+
+=item L<NKC::Transform::BIBFRAME2MARC::Utils>
+
+Utilities for bibframe2marc transformations.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/NKC-Transform-BIBFRAME2MARC>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2024-2025 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.02
+
+=cut
